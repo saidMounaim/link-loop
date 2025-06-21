2b7c61c69b441ec2adcb5163fab600c9bd5fb33d
saidmounaim <saidmounaim00@gmail.com>
2025-06-21 23:43:45 +0100

    Update footer

--- components/shared/sections/footer.tsx
+++ components/shared/sections/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="container mx-auto px-4 py-8 border-t border-gray-200">
       <div className="text-center text-gray-600">
         <p>
-          &copy; {new Date().getFullYear()} LinkTree. Made with{" "}
+          &copy; {new Date().getFullYear()} LinkLoop. Made with{" "}
           <span role="img" aria-label="love">
             ❤️
           </span>{" "}
